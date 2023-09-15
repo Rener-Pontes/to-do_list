a1e5b11b81eed301949ce57fe2c869a41c2170f0
Rener-Pontes <renercpontes@gmail.com>
2023-09-14 22:02:14 -0300

    Create a new Exception
    Create and implement the new exceptions:
        FileCreationException;
        BaseDirectoryCreationException;
    Also fixed the duplicated implementation of FileOpenException's methods

--- src/exceptions/exceptions.cpp
+++ src/exceptions/exceptions.cpp
@@ -8,7 +8,17 @@ const char* HomePathNotFoundedException::what() const throw() {
 	return message_.c_str();
 }
 
+BaseDirectoryCreationException::BaseDirectoryCreationException(const std::string& message): message_(message) {}
+const char* BaseDirectoryCreationException::what() const throw() {
+	return message_.c_str();
+}
+
 FileOpenException::FileOpenException(const std::string& message): message_(message) {}
 const char* FileOpenException::what() const throw() {
+	return message_.c_str();
+}
+
+FileCreationException::FileCreationException(const std::string& message): message_(message) {}
+const char* FileCreationException::what() const throw() {
 	return message_.c_str();
 }
